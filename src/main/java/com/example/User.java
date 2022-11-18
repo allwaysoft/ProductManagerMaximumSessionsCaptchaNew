d27666f45516995f1dfba9a91958c4c2185cde8e
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2022-11-18 15:54:13 +0800

    Add files via upload

---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,68 @@
+package com.example;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Data
+@Entity
+@DynamicUpdate
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private boolean enabled;
+
+    private String homepage;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @Column(name = "password_changed_time")
+    private Date passwordChangedTime;
+
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked;
+
+    @Column(name = "failed_attempt")
+    private int failedAttempt;
+
+    @Column(name = "lock_time")
+    private Date lockTime;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "user_history",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "history_id")
+    )
+    private Set<History> historys = new HashSet<>();
+
+}
